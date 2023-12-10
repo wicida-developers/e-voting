88406f79f63aac4d5d261266832ccf6f90f63938
Faidil <faidilfamozzy@gmail.com>
2023-12-10 12:38:36 +0800

    refactor response data in /api/polls endpoint

--- server/index.js
+++ server/index.js
@@ -24,7 +24,11 @@ app.get("/api/polls", async (_, res) => {
   try {
     const polls = await Poll.find()
     const data = polls.map(({ id, count }) => ({ [id]: count }))
-    res.send({ status: 200, message: "Data berhasil ditemukan", data })
+    res.send({
+      status: 200,
+      message: "Data berhasil ditemukan",
+      data: Object.assign({}, ...data),
+    })
   } catch (err) {
     res.status(400).send({ status: 400, message: err.message })
   }
@@ -91,27 +95,6 @@ app.post("/api/choose", auth, async (req, res) => {
     res.status(400).send({ status: 400, message: err.message })
   }
 })
-
-// app.put("/api/mhs/:nim", async (req, res) => {
-//   const { nim } = req.params
-//   const { pilihan } = req.body
-//   try {
-//     if (!nim) throw new Error("NIM tidak boleh kosong")
-//     if (!pilihan) throw new Error("Pilihan tidak boleh kosong")
-
-//     const mahasiswa = await Mahasiswa.findOne({ nim })
-
-//     if (!mahasiswa) throw new Error("NIM anda tidak ditemukan")
-//     if (mahasiswa.pilihan) throw new Error(`${nim} sudah memilih`)
-
-//     mahasiswa.pilihan = pilihan
-//     await mahasiswa.save()
-
-//     res.send({ status: 200, message: "Data berhasil diubah" })
-//   } catch (err) {
-//     res.status(400).send({ status: 400, message: err.message })
-//   }
-// })
 
 app.all("*", (_, res) => res.status(404).send({ message: "Not Found!" }))
 
