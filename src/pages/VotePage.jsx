87c6f18b445e8dfab522eebdccd4c86d22b19fdb
rizrmdhn <rizrmdhn.work@gmail.com>
2023-12-11 17:38:46 +0800

    Update styling and layout in VotePage.jsx

--- src/pages/VotePage.jsx
+++ src/pages/VotePage.jsx
@@ -37,7 +37,7 @@ export default function VotePage() {
   }
 
   return (
-    <main className="min-h-screen bg-blue-400 grid place-items-center px-8 md:px-0">
+    <main className="min-h-screen bg-blue-400 grid place-items-center px-8 py-4 md:px-0 md:py-0">
       <section className="max-w-2xl w-full text-center bg-gray-50 py-12 px-9 rounded-lg mx-8 md:shadow-md md:border md:border-gray-200">
         <h1 className="text-xl font-bold mb-10 md:text-2xl lg:text-3xl">
           PEMILIHAN CALON KETUA UMUM
@@ -45,20 +45,20 @@ export default function VotePage() {
           HIMA-TI STMIK WIDYA CIPTA DHARMA <br /> PERIODE 2023/2024
         </h1>
         <div>
-          <div className="flex gap-5 w-full rounded">
+          <div className="flex gap-5 w-full rounded flex-col items-center md:flex-row">
             {voteStatus ? (
               <h1 className="m-auto font-medium">Terima kasih telah berpartisipasi</h1>
             ) : (
               <>
                 <button
-                  className="relative bg-red-300 h-[330px] w-1/2 hover:scale-105 transition-transform"
+                  className="relative bg-red-300 h-[330px] w-full hover:scale-105 md:w-1/2 transition-transform"
                   value={"poll-1"}
                   onClick={handleVote}
                 >
                   <img src="" alt="" />
                 </button>
                 <button
-                  className="relative bg-red-300 h-[330px] w-1/2 hover:scale-105 transition-transform"
+                  className="relative bg-red-300 h-[330px] w-full hover:scale-105 md:w-1/2 transition-transform"
                   value={"poll-2"}
                   onClick={handleVote}
                 >
