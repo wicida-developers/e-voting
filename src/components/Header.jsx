f6baff1731dcbcc287d3d0999bac0732d524ae03
MuhammadRadifa <m.difa123456@gmail.com>
2023-12-12 07:42:26 +0800

    fix header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,23 +8,26 @@ export default function Header() {
 
   const navigate = useNavigate()
   return (
-    <div className="header flex flex-row justify-between items-center w-full h-20 ">
+    <div
+      className="header flex flex-row fixed bg-gray-200 rounded-md bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-40 
+    top-0 z-10 px-5 xl:px-10 justify-between items-center w-full h-20 "
+    >
       <div className="logo flex flex-row items-center">
-        <img src={HimaTiLogo} alt="HimaTi Logo" className="h-10 w-10" />
+        <img src={HimaTiLogo} alt="HimaTi Logo" className="w-10 md:w-16" />
         <div className="logo-text ml-2 text-gray-700 font-bold text-lg"></div>
       </div>
       <div className="button flex flex-row items-center">
         {authUser ? (
           <Button
             onClick={() => navigate("/vote")}
-            className="bg-white text-loginBg border-loginBg border-2 shadow-none"
+            className="bg-loginBg text-white border-whitext-white border-2 shadow-none "
           >
             Vote
           </Button>
         ) : (
           <Button
             onClick={() => navigate("/login")}
-            className="bg-white text-loginBg border-loginBg border-2 shadow-none"
+            className="bg-loginBg text-white border-loginBg border-2 shadow-none "
           >
             Login
           </Button>
