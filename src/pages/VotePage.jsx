18de9daa13bd06b50df40875752c0121f5086e2e
rizrmdhn <rizrmdhn.work@gmail.com>
2023-12-12 17:28:04 +0800

    Refactor VotePage component and handle error messages

--- src/pages/VotePage.jsx
+++ src/pages/VotePage.jsx
@@ -2,6 +2,7 @@ import Swal from "sweetalert2"
 import withReactContent from "sweetalert2-react-content"
 import { useDispatch, useSelector } from "react-redux"
 import { asyncChoose } from "../states/choose/action"
+import myToast from "../components/MyToast"
 
 export default function VotePage() {
   const authUser = useSelector((state) => state.authUser)
@@ -38,28 +39,26 @@ export default function VotePage() {
           confirmButtonText: "Ok",
         })
 
-        console.log(value)
         if (secondConfirmation.isConfirmed) {
-          await api.choose(value)
-          setVoteStatus(true)
+          dispatch(asyncChoose(id))
         }
       }
     } catch (err) {
-      console.error(err)
+      myToast.fire({ icon: "error", title: err.message })
     }
   }
 
   return (
     <main className="min-h-screen bg-blue-400 grid place-items-center px-2 py-4 md:px-0 md:py-0">
       <section className="max-w-2xl w-full text-center bg-gray-50 py-12 px-6 rounded-lg mx-8 md:shadow-md md:border md:border-gray-200">
-        <h1 className="text-lg font-bold mb-10 md:text-2xl lg:text-3xl">
+        <h1 className="text-xl font-bold mb-10 md:text-2xl lg:text-3xl">
           PEMILIHAN CALON KETUA UMUM
           <br />
-          Himpunan Mahasiswa Teknik Informatika <br /> PERIODE 2023/2024
+          HIMA-TI STMIK WIDYA CIPTA DHARMA <br /> PERIODE 2023/2024
         </h1>
         <div>
           <div className="flex gap-5 w-full rounded flex-col items-center sm:flex-row">
-            {voteStatus ? (
+            {authUser?.chosen ? (
               <>
                 <div className="m-auto space-y-2">
                   <span className="text-4xl">👍</span>
@@ -68,20 +67,16 @@ export default function VotePage() {
               </>
             ) : (
               <>
-                <button
-                  className="relative bg-red-300 h-[330px] w-full hover:scale-105 md:w-1/2 transition-transform"
-                  value={"poll-1"}
-                  onClick={handleVote}
-                >
-                  <img src="" alt="" />
-                </button>
-                <button
-                  className="relative bg-red-300 h-[330px] w-full hover:scale-105 md:w-1/2 transition-transform"
-                  value={"poll-2"}
-                  onClick={handleVote}
-                >
-                  <img src="" alt="" />
-                </button>
+                {dataCandidate.map((data, index) => (
+                  <button
+                    key={index}
+                    className="relative bg-red-300 h-[330px] w-full hover:scale-105 md:w-1/2 transition-transform"
+                    value={data.id}
+                    onClick={() => handleVote(data.id)}
+                  >
+                    <img src={data.image} className="w-full h-full object-cover" />
+                  </button>
+                ))}
               </>
             )}
           </div>
